f27eca0d29d07bff4824d3031e5e6e19f298104b
temi-tope <temitope.ogungbesan.5@gmail.com>
2019-12-04 11:24:29 +0100

    component, binding

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was craeted!';
+  serverName = 'Testserver';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+    ngOnInit() {
+  }
+    onCreateServer() {
+      this.serverCreationStatus = 'Server was created! Name is' + this.serverName;
+  }
+    onUpdateServerName(event: any) {
+      this.serverName = (event.target as HTMLInputElement).value;
+    }
+}
+
+
