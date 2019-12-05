54216d37f3e8dd1006c55f511d7085431b4c110c
temi-tope <temitope.ogungbesan.5@gmail.com>
2019-12-05 15:02:28 +0100

    Making click on a recipe work

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     // tslint:disable-next-line: max-line-length
     new Recipe('A test Recipe', 'This is just a test', 'https://tse3.mm.bing.net/th?id=OIP.4suPi4X0P8OSQwSJ4KE5wQHaLH&pid=Api&P=0&w=300&h=300'),
@@ -19,6 +20,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+
   }
 
 }
